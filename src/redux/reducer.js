61e0516c8e6e14c02d79ce27c3803b6f0109b395
Jared Baur <jaredbaur@fastmail.com>
2020-03-30 11:25:28 -0700

    redux init finished

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,33 @@
 const initialState = {
-  color: "#000"
+  color: "#000",
+  thickness: "1px"
 };
 
-const GET_COLOR = "GET_COLOR";
 const SET_COLOR = "SET_COLOR";
+const SET_THICKNESS = "SET_THICKNESS";
 
-export function getColor() {
+export function getSettings() {}
+
+export function setColor(color) {
   return {
-    type: GET_COLOR,
-    payload: initialState.color
+    type: SET_COLOR,
+    payload: color
+  };
+}
+
+export function setThickness(thickness) {
+  return {
+    type: SET_THICKNESS,
+    payload: thickness
   };
 }
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case GET_COLOR:
-      return state;
     case SET_COLOR:
       return { ...state, color: action.payload };
+    case SET_THICKNESS:
+      return { ...state, thickness: action.payload };
     default:
       return state;
   }
