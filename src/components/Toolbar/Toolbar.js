0f7d31de5f08a0335c879e59e13cd339fa9a3f9e
dotrider <56666461+dotrider@users.noreply.github.com>
2020-03-31 09:40:33 -0700

    uninstalled react-color

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import {SwatchesPicker} from 'react-color';
 import './Toolbar.css';
 
 
 function Toolbar() {
-    //state
-    // const [color, setColor] = useState('#ff0000')
 
     //mapToProps
     const color = useSelector(state => state.color)
@@ -15,10 +12,6 @@ function Toolbar() {
     //dispatchToProps
     const dispatch = useDispatch()
 
-    //swatches
-    // const handleColorChange = (color) => {
-    //     setColor(color.hex)
-    // }
 
     const handleColorChange = (e) => {
         dispatch({type: e.target.name, payload: e.target.value})
@@ -32,14 +25,10 @@ function Toolbar() {
         <div className='picker'>
             <h2>Pick a Color!</h2>
             <input type='color' name='SET_COLOR' value={color} onChange={handleColorChange}/>
-            
-            {/* <SwatchesPicker
-            width= '175px'
-            height= '350px'
-            color={color}
-            triangle='hide'
-            onChangeComplete={handleColorChange}
-            />     */}
+
+        </div>
+        <div>
+            <input></input>
         </div>
 
         <div className='buttons'>
