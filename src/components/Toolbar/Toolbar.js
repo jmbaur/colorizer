fba2fd3f71b563270ea8dc21555f935c9b1a5af8
dotrider <56666461+dotrider@users.noreply.github.com>
2020-03-30 17:14:34 -0700

    tool redux

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,28 +1,47 @@
-import React, {useState} from 'react';
-import {SwatchesPicker} from 'react-color';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import {SwatchesPicker} from 'react-color';
 import './Toolbar.css';
 
 
 function Toolbar() {
-    const [color, setColor] = useState('#000')
+    //state
+    // const [color, setColor] = useState('#ff0000')
 
+    //mapToProps
+    const color = useSelector(state => state.color)
+    console.log('intialState', color)
 
-    const handleColorChange = (color) => {
-        setColor(color.hex)
+    //dispatchToProps
+    const dispatch = useDispatch()
+
+    //swatches
+    // const handleColorChange = (color) => {
+    //     setColor(color.hex)
+    // }
+
+    const handleColorChange = (e) => {
+        dispatch({type: e.target.name, payload: e.target.value})
     }
+
+
+
 
     return (
         <section className='toolbar'>
         <div className='picker'>
             <h2>Pick a Color!</h2>
-            <SwatchesPicker
+            <input type='color' name='SET_COLOR' value={color} onChange={handleColorChange}/>
+            
+            {/* <SwatchesPicker
             width= '175px'
             height= '350px'
             color={color}
             triangle='hide'
             onChangeComplete={handleColorChange}
-            />   
+            />     */}
         </div>
+
         <div className='buttons'>
             <button className='Btn'>Undo</button> 
             <br/><br/>
