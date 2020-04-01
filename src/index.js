88ef1254ca67871ec36504e38225ebdbef5358e4
dotrider <56666461+dotrider@users.noreply.github.com>
2020-04-01 09:05:38 -0700

    buttons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+
       <App />
-    </Provider>
+
   </React.StrictMode>,
   document.getElementById("root")
 );
