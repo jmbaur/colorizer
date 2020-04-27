07bac1adaa8203e704cdfc89b182a0cb2f3bf24a
Jared Baur <jaredbaur@fastmail.com>
2020-04-26 17:22:28 -0700

    strict samesite spec in backend server

--- server/index.js
+++ server/index.js
@@ -49,6 +49,7 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET || "secret",
     resave: false,
+    sameSite: true,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } // 1 week
   })
