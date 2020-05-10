442a492728f47038bd9885353e2511ab4a75aa77
Jared Baur <jaredbaur@fastmail.com>
2020-05-10 03:44:01 +0000

    docker link name changed for socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 const socket =
   process.env.NODE_ENV === "production"
     ? io(`${process.env.REACT_APP_URL}/`)
-    : io("http://192.168.1.10:8001/");
+    : io("http://api:8001/");
 
 const socketInst = React.createContext(socket);
 const { Provider } = socketInst;
